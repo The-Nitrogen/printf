650c1871f11e3f6079da43937d8a9846d6aa3b2a
The-Nitrogen <choaibderiham0000@gmail.com>
2023-10-16 16:57:43 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
-void printf_buffer(char buffer[], int *printf_ind);
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * my_printf - Printf function
+ * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
-int my_printf(const char *format, ...)
+int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_count = 0;
-	int flags, width, precision, size, printf_ind = 0;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
@@ -23,15 +23,15 @@ int my_printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			buffer[printf_ind++] = format[i];
-			if (printf_ind == BUFF_SIZE)
-				printf_buffer(buffer, &printf_ind);
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
 			/* write(1, &format[i], 1);*/
-			printed_count++;
+			printed_chars++;
 		}
 		else
 		{
-			printf_buffer(buffer, &printf_ind);
+			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
@@ -41,13 +41,26 @@ int my_printf(const char *format, ...)
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
-			printed_count += printed;
+			printed_chars += printed;
 		}
 	}
 
-	printf_buffer(buffer, &printf_ind);
+	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
 
-	return (printed_count);
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
